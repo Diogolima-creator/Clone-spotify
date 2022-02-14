ce6bbcb019b9ef74697a5c801449ec67cfb02f49
Diogolima <Dbznetudo@gmail.com>
2022-02-14 08:26:24 -0400

    teste

--- clone-spotify/clone-spotify-2/lib/spotify.js
+++ clone-spotify/clone-spotify-2/lib/spotify.js
@@ -25,8 +25,8 @@ const queryParamString = new URLSearchParams(params);
 const LOGIN_URL = "https://accounts.spotify.com/authorize?" + queryParamString.toString();
 
 const SpotifyApi = new SpotifyWebApi({
-    clientId: process.env.NEXT_PUBLICK_CLIENT_ID,
-    clientSecret: process.env.NEXT_PUBLICK_CLIENT_SECRET,
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
 });
 
 export default SpotifyApi;
